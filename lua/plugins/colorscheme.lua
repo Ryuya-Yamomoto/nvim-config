a988a222c47ce996a9b1cc6931a00d5b9a52b7ad
Ryuya Yamamoto <ryuya.yamamoto.0325@gmail.com>
2026-02-02 00:02:40 +0900

    colorsheme 修正

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,21 +1,21 @@
-return {
-  'everviolet/nvim', name = 'evergarden',
-  priority = 1000, -- Colorscheme plugin is loaded first before any other plugins
-  opts = {
-    theme = {
-      variant = 'fall', -- 'winter'|'fall'|'spring'|'summer'
-      accent = 'green',
-    },
-    editor = {
-      transparent_background = false,
-      sign = { color = 'none' },
-      float = {
-        color = 'mantle',
-        solid_border = false,
-      },
-      completion = {
-        color = 'surface0',
-      },
-    },
-  }
+return {
+  'everviolet/nvim', name = 'evergarden',
+  priority = 1000,
+  opts = {
+    theme = {
+      variant = 'fall',
+      accent = 'green',
+    },
+    editor = {
+      transparent_background = true,
+      sign = { color = 'none' },
+      float = {
+        color = 'none',  -- フロートウィンドウも透過
+        solid_border = false,
+      },
+      completion = {
+        color = 'none',  -- 補完ウィンドウも透過
+      },
+    },
+  }
 }
