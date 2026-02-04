854da65e7d4f442faaf44d6b641c94c12ec2b1ea
ryuya yamamoto <ryuya.yamamoto@caters.co.jp>
2026-02-04 19:06:10 +0900

    keymaps LSP系コマンド設定

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,22 +1,13 @@
--- クリップボード操作のキーマップ
 local keymap = vim.keymap.set
 
--- ノーマルモード・ビジュアルモード共通
--- <leader>y: 選択範囲をクリップボードにコピー
+-- クリップボード操作のキーマップ
 keymap({"n", "v"}, "<leader>y", '"+y', { desc = "Yank to clipboard" })
-
--- <leader>d: 選択範囲を切り取ってクリップボードにコピー
 keymap({"n", "v"}, "<leader>d", '"+d', { desc = "Delete to clipboard" })
-
--- <leader>p: クリップボードから貼り付け
 keymap({"n", "v"}, "<leader>p", '"+p', { desc = "Paste from clipboard" })
-
--- 追加で便利なマッピング
--- <leader>Y: 行全体をクリップボードにコピー
 keymap("n", "<leader>Y", '"+Y', { desc = "Yank line to clipboard" })
-
--- <leader>P: カーソル前にクリップボードから貼り付け
 keymap({"n", "v"}, "<leader>P", '"+P', { desc = "Paste before from clipboard" })
+keymap("i", "jj", "<Esc>")
 
--- <Esc> キーマッピング
-vim.keymap.set("i", "jj", "<Esc>")
+-- LSP関係のジャンプ機能
+keymap('n', '<leader>gd', '<cmd>Telescope lsp_definitions<CR>', { desc = '定義ジャンプ (Telescope)' })
+keymap('n', '<leader>gr', '<cmd>Telescope lsp_references<CR>', { desc = '参照一覧 (Telescope)' })
