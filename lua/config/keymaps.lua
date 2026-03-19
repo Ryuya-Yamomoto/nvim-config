23a25934a0986977489a65aa808bf6fd9f4c0b2c
ryuya yamamoto <ryuya.yamamoto@caters.co.jp>
2026-03-19 11:50:53 +0900

    エラー内容表示

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -9,6 +9,11 @@ keymap({"n", "v"}, "<leader>P", '"+P', { desc = "Paste before from clipboard" })
 keymap("n", "<leader>cp", ':let @+ = expand("%:p")<CR>', { desc = "Copy file path to clipboard" })
 keymap("i", "jj", "<Esc>")
 
+-- 診断（エラー表示）
+keymap('n', '<leader>e', vim.diagnostic.open_float, { desc = 'エラー詳細表示' })
+keymap('n', '[d', vim.diagnostic.goto_prev, { desc = '前の診断へ' })
+keymap('n', ']d', vim.diagnostic.goto_next, { desc = '次の診断へ' })
+
 -- LSP関係のジャンプ機能
 keymap('n', 'gd', '<cmd>Telescope lsp_definitions<CR>', { desc = '定義ジャンプ' })
 keymap('n', 'gr', '<cmd>Telescope lsp_references<CR>', { desc = '参照一覧' })
